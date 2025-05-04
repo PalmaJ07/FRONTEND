3704f1105817dd570d6bf19c92f32cf1cb237ba6
ramon.sosa <rsosa@tihouseni.com>
2025-05-04 11:00:47 -0600

    Actualizacion Sidebar v2

--- FRONTEND/src/components/layout/DashboardLayout.tsx
+++ FRONTEND/src/components/layout/DashboardLayout.tsx
@@ -4,11 +4,13 @@ import { Navbar } from "./Navbar";
 
 export function DashboardLayout() {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
+    <div className="flex h-screen">
+      <aside className="w-64 bg-gray-800 text-white h-screen sticky top-0">
+        <Sidebar />
+      </aside>
       <div className="flex-1 flex flex-col">
         <Navbar />
-        <main className="flex-1 bg-gray-100 p-6">
+        <main className="flex-1 bg-gray-100 p-6 overflow-y-auto">
           <Outlet />
         </main>
       </div>
